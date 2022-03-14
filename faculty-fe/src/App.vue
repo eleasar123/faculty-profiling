fdaa4988d795452a87e80d4d845b754052dc1120
Jolly1223 <jolly.borbon2001@gmail.com>
2022-03-14 21:13:57 +0800

    added

---
+++ faculty-fe/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+
+
+export default{
+  name: 'App',
+  // components: () => {
+    
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
